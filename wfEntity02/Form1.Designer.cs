08572f5b7255b358d05bc02c4d339e792a7df87a
Ahmet Çankal <acankal@gmail.com>
2022-10-27 13:27:26 +0300

    2710

--- wfEntity02/Form1.Designer.cs
+++ wfEntity02/Form1.Designer.cs
@@ -43,6 +43,9 @@ namespace wfEntity02
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.txturunid = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnjoin = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
@@ -50,14 +53,14 @@ namespace wfEntity02
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 127);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 166);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(776, 314);
+            this.dataGridView1.Size = new System.Drawing.Size(776, 275);
             this.dataGridView1.TabIndex = 0;
             // 
             // btnadd
             // 
-            this.btnadd.Location = new System.Drawing.Point(363, 13);
+            this.btnadd.Location = new System.Drawing.Point(363, 30);
             this.btnadd.Name = "btnadd";
             this.btnadd.Size = new System.Drawing.Size(75, 23);
             this.btnadd.TabIndex = 1;
@@ -67,23 +70,25 @@ namespace wfEntity02
             // 
             // txturunad
             // 
-            this.txturunad.Location = new System.Drawing.Point(139, 13);
+            this.txturunad.Location = new System.Drawing.Point(139, 30);
             this.txturunad.Name = "txturunad";
             this.txturunad.Size = new System.Drawing.Size(195, 20);
             this.txturunad.TabIndex = 2;
+            this.txturunad.TextChanged += new System.EventHandler(this.Search_Click);
             // 
             // Delete
             // 
-            this.Delete.Location = new System.Drawing.Point(363, 42);
+            this.Delete.Location = new System.Drawing.Point(363, 59);
             this.Delete.Name = "Delete";
             this.Delete.Size = new System.Drawing.Size(75, 23);
             this.Delete.TabIndex = 1;
             this.Delete.Text = "Delete";
             this.Delete.UseVisualStyleBackColor = true;
+            this.Delete.Click += new System.EventHandler(this.Delete_Click);
             // 
             // List
             // 
-            this.List.Location = new System.Drawing.Point(363, 71);
+            this.List.Location = new System.Drawing.Point(363, 88);
             this.List.Name = "List";
             this.List.Size = new System.Drawing.Size(75, 23);
             this.List.TabIndex = 1;
@@ -92,17 +97,18 @@ namespace wfEntity02
             // 
             // Search
             // 
-            this.Search.Location = new System.Drawing.Point(444, 13);
+            this.Search.Location = new System.Drawing.Point(444, 30);
             this.Search.Name = "Search";
             this.Search.Size = new System.Drawing.Size(75, 23);
             this.Search.TabIndex = 1;
             this.Search.Text = "Search";
             this.Search.UseVisualStyleBackColor = true;
+            this.Search.Click += new System.EventHandler(this.Search_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(37, 19);
+            this.label1.Location = new System.Drawing.Point(37, 36);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(48, 13);
             this.label1.TabIndex = 3;
@@ -110,23 +116,25 @@ namespace wfEntity02
             // 
             // comboBox1
             // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(139, 40);
+            this.comboBox1.Location = new System.Drawing.Point(139, 57);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(195, 21);
             this.comboBox1.TabIndex = 4;
             // 
             // comboBox2
             // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(139, 73);
+            this.comboBox2.Location = new System.Drawing.Point(139, 90);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(195, 21);
             this.comboBox2.TabIndex = 5;
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(139, 101);
+            this.numericUpDown1.Location = new System.Drawing.Point(139, 118);
             this.numericUpDown1.Name = "numericUpDown1";
             this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
             this.numericUpDown1.TabIndex = 6;
@@ -134,7 +142,7 @@ namespace wfEntity02
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(58, 18);
+            this.label2.Location = new System.Drawing.Point(58, 35);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(48, 13);
             this.label2.TabIndex = 3;
@@ -143,7 +151,7 @@ namespace wfEntity02
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(46, 42);
+            this.label3.Location = new System.Drawing.Point(46, 59);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(46, 13);
             this.label3.TabIndex = 3;
@@ -152,7 +160,7 @@ namespace wfEntity02
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(58, 76);
+            this.label4.Location = new System.Drawing.Point(58, 93);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(51, 13);
             this.label4.TabIndex = 3;
@@ -161,25 +169,55 @@ namespace wfEntity02
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(58, 103);
+            this.label5.Location = new System.Drawing.Point(58, 120);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(29, 13);
             this.label5.TabIndex = 3;
             this.label5.Text = "Fiyat";
+            // 
+            // txturunid
+            // 
+            this.txturunid.Location = new System.Drawing.Point(139, 4);
+            this.txturunid.Name = "txturunid";
+            this.txturunid.Size = new System.Drawing.Size(195, 20);
+            this.txturunid.TabIndex = 2;
+            this.txturunid.TextChanged += new System.EventHandler(this.Search_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(58, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(77, 13);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Ürün Numarası";
+            // 
+            // btnjoin
+            // 
+            this.btnjoin.Location = new System.Drawing.Point(444, 59);
+            this.btnjoin.Name = "btnjoin";
+            this.btnjoin.Size = new System.Drawing.Size(75, 23);
+            this.btnjoin.TabIndex = 7;
+            this.btnjoin.Text = "Join";
+            this.btnjoin.UseVisualStyleBackColor = true;
+            this.btnjoin.Click += new System.EventHandler(this.btnjoin_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnjoin);
             this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.txturunid);
             this.Controls.Add(this.txturunad);
             this.Controls.Add(this.List);
             this.Controls.Add(this.Search);
@@ -212,6 +250,9 @@ namespace wfEntity02
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txturunid;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnjoin;
     }
 }
 
